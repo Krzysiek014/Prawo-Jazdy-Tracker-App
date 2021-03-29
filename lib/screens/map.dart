2141279cbe4eb246925f68940fe4c0dc2329fb2e
Krzysiek014 <krzysiek014@o2.pl>
2021-03-29 18:34:16 +0200

    Add -> Floating Action Button for GPS turn on/off on map screen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -5,36 +5,56 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:location_permissions/location_permissions.dart';
 import 'package:prawo_jazdy_app/main.dart';
 
-class MapHome extends StatelessWidget {
+class MapHome extends StatefulWidget {
+  @override
+  _MapHomeState createState() => _MapHomeState();
+}
+
+class _MapHomeState extends State<MapHome> {
+  IconData gps_state = Icons.gps_off;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'MAPA',
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, ProfileRoute);
-                },
-                icon: Icon(
-                  Icons.account_circle,
-                  color: Colors.white,
-                ))
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'MAPA',
         ),
-        body: Container(
-          child: MapboxMap(
-            initialCameraPosition: const CameraPosition(
-                target: LatLng(52.16253, 21.038477), zoom: 14.0),
-            accessToken: getKey(context, 'MAPBOX_APIKEY').toString(),
-            myLocationEnabled: true,
-            myLocationRenderMode: MyLocationRenderMode.GPS,
-            myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
-          ),
-        ));
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, ProfileRoute);
+              },
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Container(
+        child: MapboxMap(
+          initialCameraPosition: const CameraPosition(
+              target: LatLng(52.16253, 21.038477), zoom: 14.0),
+          accessToken: getKey(context, 'MAPBOX_APIKEY').toString(),
+          myLocationEnabled: true,
+          myLocationRenderMode: MyLocationRenderMode.GPS,
+          myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(gps_state),
+        onPressed: () {
+          setState(() {
+            if (gps_state == Icons.gps_off) {
+              gps_state = gps_state = Icons.gps_fixed;
+            } else {
+              gps_state = gps_state = Icons.gps_off;
+            }
+          });
+        },
+      ),
+    );
   }
 
   getKey(BuildContext context, String name) async {
